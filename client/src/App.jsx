fe1f40d919144943d003f93dc970cabbd215cf50
Diego Vilhlava <diegorocha450@yahoo.com>
2024-03-25 11:39:18 -0300

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import DashBoard from "./pages/DashBoard"
+import Projects from "./pages/Projects"
 
 function App() {
   
   return (
-    <></>
+    <BrowserRouter>
+    <Routes>
+    <Route />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<DashBoard/>} />
+        <Route path="/projects" element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
