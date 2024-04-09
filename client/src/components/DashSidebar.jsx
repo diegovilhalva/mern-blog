73ff5e6caed4757bf2677f7a4d8506b9fdbffb8b
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-09 13:41:13 -0300

    add dashboard overview to the admain dash baod

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,10 +1,10 @@
 import { Sidebar } from "flowbite-react"
 import { useEffect, useState } from "react"
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi"
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi"
 import { useDispatch,useSelector } from "react-redux"
 import { Link, useLocation } from "react-router-dom"
 import { signoutSuccess } from "../redux/user/userSlice"
-import { current } from "@reduxjs/toolkit"
+
 
 const DashSidebar = () => {
     const location = useLocation()
@@ -40,6 +40,15 @@ const DashSidebar = () => {
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
                 <Sidebar.ItemGroup className="flex flex-col gap-1">
+                  {
+                    currentUser && currentUser.isAdmin && 
+                    <Link to={'/dashboard?tab=dashboard'}>
+                      <Sidebar.Item active={tab === 'dashboard' || !tab} icon={HiChartPie} as="div" >
+                        Dashboard
+                      </Sidebar.Item>
+
+                    </Link>
+                  }
                     <Link to={'/dashboard?tab=profile'}>
                         <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor={'dark'} as='div'>
                             Meu Perfil
--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile"
 import DashPosts from "../components/DashPosts"
 import DashUsers from "../components/DashUsers"
 import DashComments from "../components/DashComments"
+import DashboardCom from "../components/DashboardCom"
 
 const DashBoard = () => {
   const location = useLocation()
@@ -25,6 +26,7 @@ const DashBoard = () => {
       {tab === 'posts' && <DashPosts/>}
       {tab === 'users' && <DashUsers/>}
       {tab === 'comments' && <DashComments/>}
+      {tab === 'dashboard' && <DashboardCom/>}
     </div>
   )
 }
