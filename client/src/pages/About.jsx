cca5c66e194b0633328cc8503d45e860e6fe8365
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-10 14:02:19 -0300

    complete about project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,20 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center flex-col gap-6">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">Sobre  o Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>  Aqui você encontrará um universo de conhecimento e prática para todos os entusiastas da programação, desde iniciantes até desenvolvedores experientes em busca de inspiração. Explore uma variedade de tópicos, desde linguagens de programação populares como Python, JavaScript e Java, até frameworks modernos, conceitos avançados de algoritmos e dicas práticas para aprimorar suas habilidades de codificação.
+            </p> 
+            <p>
+              Estou comprometido em fornecer conteúdo acessível e envolvente, repleto de tutoriais passo a passo, análises de tendências da indústria e insights valiosos para ajudá-lo a se destacar no mundo da tecnologia. Junte-se a mim nesta jornada emocionante de aprendizado e crescimento no universo da programação.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,14 @@
 
-
+import CallToAction from "../components/CallToAction"
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center p-3 flex-col ">
+      <h1 className="text-3xl font-semibold">Projetos</h1>
+      <p className="text-md text-gray-500 mb-3">
+        Descubra um mundo de oportunidades criativas e desafios estimulantes no nosso link para projetos de programação! Navegue por uma coleção diversificada de ideias e inspirações para seus próximos projetos de codificação, desde aplicativos simples até sistemas complexos. Explore recursos úteis, tutoriais detalhados e exemplos de código para ajudá-lo a dar vida às suas ideias. Esteja você procurando aprimorar suas habilidades, colaborar com outros desenvolvedores ou simplesmente se aventurar em novos territórios de programação, nosso link é o ponto de partida perfeito para sua jornada criativa. Pronto para começar a transformar suas ideias em realidade? Clique agora e mergulhe em um mundo de possibilidades infinitas!
+        </p>
+        <CallToAction/>
+    </div>
   )
 }
 
